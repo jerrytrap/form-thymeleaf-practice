67945ed18ef739c4b1888c7e6396b8d1b5c78d76
류대현 <eogus1027@naver.com>
2024-12-17 17:40:16 +0900

    제목, 내용을 입력하지 않은 경우, 글자 수가 모자란 경우 처리

--- src/main/java/com/example/tem/PostController.java
+++ src/main/java/com/example/tem/PostController.java
@@ -23,6 +23,58 @@ public class PostController {
     @PostMapping("/write")
     @ResponseBody
     public String write(String title, String content) {
+        if (title == null || title.isBlank()) {
+            return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목">
+                    <br>
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <br>
+                    <input type="submit" value="생성">
+                </form>
+                """.formatted("제목을 입력하세요.");
+        }
+
+        if (title.length() < 5) {
+            return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목">
+                    <br>
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <br>
+                    <input type="submit" value="생성">
+                </form>
+                """.formatted("제목을 5자 이상 입력하세요.");
+        }
+
+        if (content == null || content.isBlank()) {
+            return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목">
+                    <br>
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <br>
+                    <input type="submit" value="생성">
+                </form>
+                """.formatted("내용을 입력하세요.");
+        }
+
+        if (content.length() < 10) {
+            return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목">
+                    <br>
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <br>
+                    <input type="submit" value="생성">
+                </form>
+                """.formatted("내용을 10자 이상 입력하세요.");
+        }
+
         return """
                 <h1>글쓰기 완료</h1>
                 
